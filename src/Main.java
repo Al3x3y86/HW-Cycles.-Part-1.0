2f9d999e0c35c2e5dfa56909d57c1f3b2b990d9c
Alexey <alexey_kuzminykh@mail.ru>
2022-10-13 12:19:17 +0300

    HW 1.2

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,21 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Интерация цикла " + i);
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println("Год является високосным " + i);
         }
 
         // Задание 2
         System.out.println("Задание 2");
-        for (int i = 10; i > 0; i--) {
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.println("Интерация цикла " + i);
         }
 
         // Задание 3
         System.out.println("Задание 3");
-        for (int i = 0; i <= 17; i+=2) {
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println("Интерация цикла " + i);
         }
 
-        // Задание 4
-        System.out.println("Задание 4");
-        for (int i = 10; i >= -10; i--) {
-            System.out.println("Интерация цикла " + i);
-        }
     }
 }
